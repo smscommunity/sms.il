3e5acc7c39502eb44570049b14d6af5f2f53ae42
John IL <saveeveryframe@github.com>
2021-09-09 00:09:52 +0000

    deploy: e202d2e49dcbf74132877a82023c8a37159d63db

---
+++ _next/static/uvT6uCgUaV4EHZKwgHL5D/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/05bc601c6fa0bd649eb5.css","static/chunks/pages/index-f5ad1cbe1ce95b211622.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
