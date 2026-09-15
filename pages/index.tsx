ab43177aa2c9d3ca5393428dc215a1163f3cd238
Theo <zeldocto@gmail.com>
2026-09-15 13:18:52 -0700

    Add files via upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,18 +12,23 @@ import Footer from '../components/Footer';
 import FilterHeader from '../components/FilterHeader';
 import PlayerTable from '../components/PlayerTable';
 import PlayerData from '../types/PlayerData';
+import CategorySortControl from '../components/CategorySortControl';
+import buildCategoryStandings from '../scripts/buildCategoryStandings';
+import { CATEGORIES } from '../data/categories';
 
 interface ILPageProps {
     ilData: ILData[][];
     levelData: LevelData[];
     playerData: PlayerData[];
+    categoryPlayerData: Record<string, PlayerData[]>;
     timestamp: number;
 }
 
 const Home: NextPage<ILPageProps> = (props: ILPageProps) => {
-    const { ilData, levelData, playerData, timestamp } = props;
+    const { ilData, levelData, playerData, categoryPlayerData, timestamp } = props;
     const dateStamp = new Date(timestamp);
     const [selectedIL, setSelectedIL] = React.useState(-1);
+    const [selectedCategory, setSelectedCategory] = React.useState<string | null>(null);
 
     let filteredIls: ILData[] = [];
     let selectedILData: LevelData | undefined;
@@ -35,12 +40,17 @@ const Home: NextPage<ILPageProps> = (props: ILPageProps) => {
         selectedILData = undefined;
         filteredIls = [];
     }
+    const displayedPlayerData =
+        selectedIL == -1 && !!selectedCategory ? categoryPlayerData[selectedCategory] : playerData;
+    const selectedCategoryLabel = CATEGORIES.find(category => category.key === selectedCategory)
+        ?.label;
     const headerText = !!selectedILData
         ? selectedILData.world +
           ' - ' +
           selectedILData.episode +
           (!!selectedILData.subCategory ? ' (' + selectedILData.subCategory + ')' : '')
-        : 'Super Mario Sunshine IL Leaderboards';
+        : 'Super Mario Sunshine IL Leaderboards' +
+          (selectedIL == -1 && !!selectedCategoryLabel ? ' - ' + selectedCategoryLabel : '');
 return (
   <>
     <Link href="/">
@@ -79,11 +89,17 @@ return (
         onSelectedILChange={setSelectedIL}
         headerText={headerText}
       />
+      {selectedIL == -1 && (
+        <CategorySortControl
+          selectedCategory={selectedCategory}
+          onSelectedCategoryChange={setSelectedCategory}
+        />
+      )}
       <div>
         {filteredIls.length > 0 ? (
           <ILTable ils={filteredIls} />
         ) : (
-          <PlayerTable players={playerData} />
+          <PlayerTable players={displayedPlayerData} />
         )}
       </div>
       <Footer dateStamp={dateStamp} />
@@ -96,6 +112,7 @@ export default Home;
 
 export const getStaticProps: GetStaticProps = async context => {
     const { ilData, levelData, playerData } = loadILXls();
+    const categoryPlayerData = buildCategoryStandings(ilData);
     const timestamp = Date.now();
     return {
         props: {
@@ -103,6 +120,7 @@ export const getStaticProps: GetStaticProps = async context => {
             levelData,
             timestamp,
             playerData,
+            categoryPlayerData,
         },
     };
 };
